61ec293fe9dbb4b767aa5baf2bc9ade9010598c7
Mario Augusto <mario_abreucardoso@hotmail.com>
2020-06-07 00:30:50 -0300

    app

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Routes from './routes';
+
+//JSX: sintaxe de XML dentro do JavaScript
+
+
+ 
+
+function App() {
+ 
+   
+  return (
+    
+     
+        <Routes/>
+      
+      
+  );
+}
+
+export default App;
